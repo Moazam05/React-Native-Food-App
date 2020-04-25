4e6ab0773f069bcaf2c55b0894fa2e6b5880416f
Moazam05 <salmanmoazam08@gmail.com>
2020-04-26 01:45:48 +0500

    Error Handling

--- App.js
+++ App.js
@@ -9,22 +9,27 @@ import yelp from "./src/api/yelp";
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
   const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
 
   const searchApi = async () => {
-    const response = await yelp.get("/search", {
-      params: {
-        limit: 50,
-        term,
-        location: "san jose",
-      },
-    });
-    setResults(response.data.businesses);
+    try {
+      const response = await yelp.get("/search", {
+        params: {
+          limit: 50,
+          term,
+          location: "san jose",
+        },
+      });
+      setResults(response.data.businesses);
+    } catch (err) {
+      setErrorMessage("Something Went Wrong");
+    }
   };
 
   return (
     <View>
       <SearchBar term={term} onTermChange={setTerm} onTermSubmit={searchApi} />
-      <Text>Home Screen</Text>
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have Found {results.length} Results </Text>
     </View>
   );
