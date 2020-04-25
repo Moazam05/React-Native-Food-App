6c491d38f5cf3bd5b9dddec0778803a44992897c
Moazam05 <salmanmoazam08@gmail.com>
2020-04-26 01:35:30 +0500

    Making the request

--- App.js
+++ App.js
@@ -4,19 +4,28 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
 import SearchBar from "./src/components/SearchBar";
+import yelp from "./src/api/yelp";
 
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
+  const [results, setResults] = useState([]);
+
+  const searchApi = async () => {
+    const response = await yelp.get("/search", {
+      params: {
+        limit: 50,
+        term,
+        location: "san jose",
+      },
+    });
+    setResults(response.data.businesses);
+  };
 
   return (
     <View>
-      <SearchBar
-        term={term}
-        onTermChange={(newTerm) => setTerm(newTerm)}
-        onTermSubmit={() => console.log("term was submitted")}
-      />
+      <SearchBar term={term} onTermChange={setTerm} onTermSubmit={searchApi} />
       <Text>Home Screen</Text>
-      <Text>{term}</Text>
+      <Text>We have Found {results.length} Results </Text>
     </View>
   );
 };
